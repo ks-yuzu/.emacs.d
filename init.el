bf7414d6b0cabb546b1339b27f681a8354b5631c
Oosako Yuuki <yuuki.oosako@kwansei.ac.jp>
2015-11-21 22:12:44 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,230 @@
+;;; Code:
+
+;;(exec-path-from-shell-initialize)
+
+(setq load-path
+      (append '(
+		"~/.emacs.d/inits/"
+		) load-path))
+
+(load "keybinding")
+
+
+;; use-package
+(unless (require 'use-package nil t)
+  (message "Use-package is not available")
+  (defmacro use-package (&rest args))
+)
+
+
+;; no startup message
+(setq inhibit-startup-message t)
+
+;; delete auto save files when finishing
+(setq delete-auto-save-files t)
+
+;; make backup-file in ~/bak
+(setq make-backup-files t)
+(setq backup-directory-alist
+	  (cons
+	   (cons "\\.*$" (expand-file-name "~/bak"))
+	   backup-directory-alist)
+	  )
+
+
+
+;; background transparent
+;(add-to-list 'default-frame-alist '(alpha . (0.75 0.75)))
+
+(if window-system
+	(progn
+	  (set-background-color "Black")
+	  (set-foreground-color "LightGray")
+	  (set-cursor-color "Gray")
+	  (set-frame-parameter nil 'alpha 85)
+	  ))
+
+
+;; syntax coloring
+(add-to-list 'auto-mode-alist '("\\.jsp\\'"  . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ll\\'"   . bison-mode))
+(add-to-list 'auto-mode-alist '("\\.yy.c\\'" . bison-mode))
+(add-to-list 'auto-mode-alist '("\\.zsh\\'"  . shell-script-mode))
+
+
+;; server start for emacs-client
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+
+;-------------------
+;      Path
+;-------------------
+(dolist (dir (list
+              "/sbin"
+              "/usr/sbin"
+              "/bin"
+              "/usr/bin"
+              "/opt/local/bin"
+              "/sw/bin"
+              "/usr/local/bin"
+              (expand-file-name "~/bin")
+              (expand-file-name "~/.emacs.d/bin")
+              ))
+ (when (and (file-exists-p dir) (not (member dir exec-path)))
+   (setenv "PATH" (concat dir ":" (getenv "PATH")))
+   (setq exec-path (append (list dir) exec-path))))
+
+
+;-------------------
+;  Editor looks
+;-------------------
+
+;; hide menubar, toolbar, and scrollbar
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+;(load-theme 'manoj-dark t) ; color-theme
+(setq cursor-type 'box)
+(add-to-list 'default-frame-alist '(cursor-type . 'box))
+;(set-cursor-color 'indianred)
+
+(blink-cursor-mode 0)      ; no blinking
+(global-hl-line-mode t)    ; highlight current line
+;(custom-set-faces '(hl-line ((t (:background "color-236")))) )
+(line-number-mode t)       ; display column number
+(show-paren-mode t)        ; enphasis paren set
+
+;------------------
+;     indent
+;------------------
+(setq-default c-basic-offset 4
+              tab-width 4
+              indent-tab-mode t)
+
+(add-hook 'c++-mode-hook '(lambda()	(c-set-style "bsd")))
+(add-hook 'c--mode-hook '(lambda() (c-set-style "bsd")))
+;(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+
+
+;-------------------------
+;     package control
+;-------------------------
+
+;; ---------- use package ----------
+;(use-package package : ensure package)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+;(package-refrech-contents)
+
+
+;; ---------- auto completion ----------
+(require 'auto-complete)
+(require 'auto-complete-config)
+;(global-auto-complete-mode t)
+(ac-config-default)
+
+
+;; ---------- parens ----------
+(require 'smartparens)
+(require 'rainbow-delimiters)
+
+;; ---------- statusbar ----------
+(require 'powerline)
+(powerline-default-theme)
+
+(global-anzu-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-search-threshold 1000)
+ '(custom-safe-themes (quote ("0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" default)))
+ '(send-mail-function (quote smtpmail-send-it)))
+
+
+;; ---------- Flycheck ----------
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'c++-mode-hook (lambda() (setq flycheck-gcc-language-standard "c++11")))
+
+
+;; ---------- Flymake ----------
+;(require 'flymake)
+;(setq flymake-gui-warnings-enabled nil)
+;(add-hook 'find-file-hook 'flymake-find-file-hook)
+;(global-set-key "\C-cd" 'flymake-display-err-mune-for-current-line)
+
+
+;; ---------- helm ----------
+(require 'helm-config)
+(helm-mode 1)
+ ;; BS mini-buf : C-h
+(define-key helm-read-file-map (kbd "C-h") 'delete-backward-char) 
+ ;; complete : TAB-key
+(define-key helm-read-file-map (kbd "<tab>") 'helm-execute-persistent-action)
+
+
+;; ---------- undo-tree ----------
+(require 'undo-tree)
+(global-undo-tree-mode t)
+(global-set-key (kbd "M-/") 'undo-tree-redo)
+
+
+;; ---------- setting in each language mode ----------
+;; markdown mode
+(require 'markdown-mode)
+(setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(add-hook 'markdown-mode-hook
+		  '(lamdba() (setq markdown-command "mdown") )
+)
+
+;; perl-mode
+(autoload 'perl-mode "perl-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.pl$" . perl-mode))
+
+;; haskell-mode
+(require 'haskell-mode)
+(autoload 'haskell-mode "haskell-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'auto-mode-alist '(".xmonad" . haskell-mode))
+(add-to-list 'auto-mode-alist '(".xmobar" . haskell-mode))
+
+;; perl
+;; (require 'plenv)
+;;(plenv-global "5.22.0")
+
+
+;; (defun flymake-perl()
+;;   (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;; 					 'flymake-create-temp-with-folder-structure))
+;; 		 (local-file (file-relative-name
+;; 					  temp-file
+;; 					  (file-name-directory buffer-file-name))))
+;; 	(list (guess-plenv-perl-path) (list "-wc" local-file))))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; visual regexp
+(require 'visual-regexp-steroids)
+(require 'pcre2el-autoloads)
+(setq vr/engine 'pcre2el)
+
+
+(load "mail")
+(load "mysql")
