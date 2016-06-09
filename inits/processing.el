ea585bda19984e1130372383667deb34ee2e9a44
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2016-06-09 17:52:00 +0900

    Add processing-mode

---
+++ inits/processing.el
@@ -0,0 +1,19 @@
+;;; Code:
+
+(require 'processing-mode)
+
+(setq processing-location "~/bin/processing/processing-java")
+(setq processing-application-dir "~/bin/processing")
+(setq processing-sketchbook-dir "~/works/processing")
+
+(add-hook 'processing-mode-hook
+          '(lambda ()
+             (setq processing-mode-code-indent-offset 4)
+(setq tab-width 4)))
+
+
+;; (define-package
+;;   "processing-snippets" "1.0.0"
+;;   "Snippets for processing-mode"
+;;   '((yasnippet "0.8.0")))
+
