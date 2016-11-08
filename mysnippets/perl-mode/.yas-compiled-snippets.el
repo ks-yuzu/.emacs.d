c2f643c6c274f7e03b915e67725b7ff890a4f132
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2016-11-08 10:55:33 +0900

    Add mysnippets

---
+++ mysnippets/perl-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `perl-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'perl-mode
+                     '(("use" "use strict;\nuse warnings;\nuse diagnostics;\nuse v5.10;\n" "use" nil nil nil "/home/yuzu/.emacs.d/mysnippets/perl-mode/use" nil nil)))
+
+
+;;; Do not edit! File generated at Tue Jul 26 18:44:45 2016
---
+++ mysnippets/snippet-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `snippet-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'snippet-mode
+                     '(("indent" "# expand-env: ((yas/indent-line 'fixed) (yas/wrap-around-region 'nil))" "indent_for_snippet" nil nil nil "/home/yuzu/.emacs.d/mysnippets/snippet-mode/indent_for_snippet" nil nil)))
+
+
+;;; Do not edit! File generated at Tue Jul 26 18:44:45 2016
