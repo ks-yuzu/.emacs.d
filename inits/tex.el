6fbab7cdc05741f65cc40668f3e6247f807bd36e
Yuuki Oosako <yuuki.oosako@kwansei.ac.jp>
2017-01-30 11:10:54 +0900

    Update tex.el

--- inits/tex.el
+++ inits/tex.el
@@ -51,8 +51,20 @@
     ("T1"   "fontenc"   t)
     ))
 
+;; 25.1.1
 (defvar tex-compile-commands 
   '(
-    ("texcompile.sh %f")
-    ("texcompile-landscape.sh %f")))
+    ("tex2pdf %f")
+    ("tex2pdf-landscape %f")))
 
+;; 24.5.1 ??
+;; (setq tex-compile-commands
+;;       '(
+;;         ("platex %b.tex && platex %b.tex && dvipdfmx %b.dvi")
+;;         ("tex2pdf %f")))
+
+;; 24.3.1 ??
+;; (setq tex-compile-commands
+;;       '(
+;;         ("platex %r.tex && platex %r.tex && dvipdfmx %r.dvi")
+;;         ("tex2pdf %f")))
