39ece763daf4a1a7372a351f878f5fb87c60c00b
yuki.osako <yuki.osako@gree.net>
2019-05-20 13:42:42 +0900

    Update _typescript.el

--- inits/78_lang_typescript.el
+++ inits/78_lang_typescript.el
@@ -1,13 +1,32 @@
-(el-get-build typescript)
-(use-package typescript)
+(el-get-build typescript-mode)
+(use-package typescript-mode)
+
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+
 (setq typescript-indent-level 2)
+
 
 ;; npm install -g clausreinke/typescript-tools
 (el-get-build tss)
 (use-package tss)
+
 (setq tss-popup-help-key "C-:")
 (setq tss-jump-to-definition-key "C->")
 (setq tss-implement-definition-key "C-c i")
+
 (tss-config-default)
 
+
+;; tide
+;; (package-install 'tide)
+;; (require 'tide)
+;; (add-hook 'typescript-mode-hook
+;; 		  (lambda ()
+;; 			(interactive)
+;; 			(tide-setup)
+;; 			(flycheck-mode +1)
+;; 			(setq flycheck-check-syntax-automatically '(save mode-enabled))
+;; 			(eldoc-mode +1)
+;; 			(tide-hl-identifier-mode +1)
+;; 			(company-mode +1)
+;; 			(global-set-key (kbd "M-*") 'tide-jump-back)))
