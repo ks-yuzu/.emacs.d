ce79f32bf60b09347cd83c64d8d5c0979be4473d
yuki.osako <yuki.osako@gree.net>
2019-05-21 12:44:39 +0900

    Fix a typo in _lang_confluence.el

--- inits/121_lang_confluence.el
+++ inits/121_lang_confluence.el
@@ -1,9 +1,6 @@
+(el-get-bundle elpa:xml-rpc)
 (el-get-bundle confluence)
+(use-package confluence)
 
-;; require がエラーになるので先に設定しておく
 (add-to-list 'auto-mode-alist '("\\.cwiki\\'" . confluence-edit-mode))
-
-;; confluence の依存パッケージ xml-rpc が無いため, エラーになる
-(require 'confluence)
-
 
