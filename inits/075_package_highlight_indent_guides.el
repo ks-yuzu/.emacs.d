efad6ad2ede29267798713e3ec097c0befa202cb
yuki.osako <yuki.osako@gree.net>
2019-05-21 18:40:05 +0900

    Add _highlight_indent_guides.el

---
+++ inits/075_package_highlight_indent_guides.el
@@ -0,0 +1,12 @@
+(el-get-bundle highlight-indent-guides)
+(use-package highlight-indent-guides
+  :diminish
+  :hook
+  ((prog-mode yaml-mode) . highlight-indent-guides-mode)
+  :custom
+  (highlight-indent-guides-auto-enabled t)
+  (highlight-indent-guides-responsive t)
+  (highlight-indent-guides-method 'column)
+)
+
+
