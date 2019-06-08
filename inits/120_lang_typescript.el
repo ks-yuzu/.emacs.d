97767691d9f1b6850e2a7be55440ab20bef0170a
Yuuki Oosako <yuuki.oosako.216@gmail.com>
2019-06-08 21:37:16 +0900

    Update to bind '.tsx' for typescript-mode

--- inits/120_lang_typescript.el
+++ inits/120_lang_typescript.el
@@ -2,6 +2,7 @@
 (use-package typescript-mode)
 
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
 
 (setq typescript-indent-level 2)
 
