7c4527e5bdc3cdb5195dd1e76a0cb9abbf29ea66
yuki.osako <yuki.osako@gree.net>
2019-12-27 13:50:54 +0900

    Set default compile command in confluence-edit-mode unless Makefile exists

--- inits/122_lang_confluence.el
+++ inits/122_lang_confluence.el
@@ -4,3 +4,9 @@
 
 (add-to-list 'auto-mode-alist '("\\.cwiki\\'" . confluence-edit-mode))
 
+(add-hook 'confluence-edit-mode-hook
+          (lambda ()
+            (unless (or (file-exists-p "makefile") (file-exists-p "Makefile"))
+              (set (make-local-variable 'compile-command)
+                   (concat "perl push.pl "
+                           (buffer-file-name))))))
