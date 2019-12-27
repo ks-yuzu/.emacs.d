54f3d0e170bfee2a990315bc4427d4ed08f74948
yuki.osako <yuki.osako@gree.net>
2019-12-27 13:48:47 +0900

    Add the keybind for helm-yas-complete

--- inits/057_package_yasnippet.el
+++ inits/057_package_yasnippet.el
@@ -5,5 +5,9 @@
         "~/.emacs.d/snippets"
         ))
 
+(el-get-bundle helm-c-yasnippet)
+(setq helm-yas-space-match-any-greedy t)
+(global-set-key (kbd "C-c y") 'helm-yas-complete)
+
 (yas-global-mode 1)
 
