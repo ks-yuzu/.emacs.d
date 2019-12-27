46613508de50ecbfbcbbe88827dd26ac60e7dec4
yuki.osako <yuki.osako@gree.net>
2019-12-27 13:46:26 +0900

    Change package for reveal from ox-reveal to org-re-reveal

--- inits/111_lang_org_reveal.el
+++ inits/111_lang_org_reveal.el
@@ -1,3 +1,11 @@
-(el-get-bundle ox-reveal)
-(use-package ox-reveal)
+;; (el-get-bundle ox-reveal)
+;; (use-package ox-reveal)
 ;; (load-library 'ox-reveal)
+
+
+
+(el-get-bundle org-re-reveal)
+(use-package org-re-reveal)
+
+
+
