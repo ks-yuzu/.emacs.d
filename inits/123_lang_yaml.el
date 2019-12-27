2ff159351c7204ed3958e868c3d90ac2f22df0e2
yuki.osako <yuki.osako@gree.net>
2019-12-27 13:42:17 +0900

    Enable lsp in yaml-mode

--- inits/123_lang_yaml.el
+++ inits/123_lang_yaml.el
@@ -1,4 +1,23 @@
-(el-get-bundle yaml-mode)
-(use-package yaml-mode)
+(use-package yaml-mode
+  :init (el-get-bundle yaml-mode))
 
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+
+
+
+;; lsp
+
+;; npm install -g yaml-language-server
+(el-get-bundle lsp-yaml)
+(use-package lsp-yaml
+  :init (el-get-bundle iquiw/lsp-yaml)
+  :after lsp
+  ;; :quelpa (lsp-yaml :fetcher github :repo "iquiw/lsp-yaml")
+  :hook (yaml-mode
+         . (lambda ()
+             (setq-local lsp-eldoc-render-all t)
+             (eldoc-mode)))
+  :config
+  (add-hook 'yaml-mode-hook #'lsp)
+  (setq lsp-yaml-schemas '(:kubernetes "*.yml")))
+
