32d1d261212049dc3284b3681fe917f4fda6ea80
yuzu <yuzu6120@gmail.com>
2021-11-29 20:21:12 +0900

    Add align keybinding in terraform mode

--- inits/127_lang_terraform.el
+++ inits/127_lang_terraform.el
@@ -1,4 +1,10 @@
 (el-get-bundle terraform-mode)
-(use-package terraform-mode)
+(use-package terraform-mode
+  :bind
+  (("C-c C-a" . (lambda (begin end)
+                  "Align region to equal signs"
+                  (interactive "r")
+                  (align-regexp begin end
+                                (rx (group (zero-or-more (syntax whitespace))) "=") 1 1 )))))
 
 
