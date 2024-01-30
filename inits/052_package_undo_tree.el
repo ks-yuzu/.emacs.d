8ec542ddccb6fbb39075b730afda6461bce0d350
yuki.osako <yuki.osako@gree.net>
2024-01-30 17:21:19 +0900

    Fix undotree config

--- inits/052_package_undo_tree.el
+++ inits/052_package_undo_tree.el
@@ -4,7 +4,7 @@
   :custom
   (undo-tree-enable-undo-in-region . nil)
   :config
-;;(global-undo-tree-mode t)
   (add-to-list 'undo-tree-history-directory-alist
-               '(expand-file-name "~/.emacs.d/undotree/")))
+               (cons "\\.*$" (expand-file-name "~/.emacs.d/undotree/")))
+  )
 
